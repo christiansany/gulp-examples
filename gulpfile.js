d32fdfa27d0ec1c89e686e1f8adc602a271787a8
Christian Sany <christian.sany@bluewin.ch>
2019-09-11 08:48:40 +0200

    Add refactored solution to new repository

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
 var gulp = require('gulp'),
 	jshint = require('gulp-jshint'),
 	concat = require('gulp-concat'),
-	uglify = require('gulp-uglify');
+	uglify = require('gulp-uglify'),
+	sass = require('gulp-sass'),
+	postCSS = require('gulp-postcss'),
+	autoprefixer = require('autoprefixer');
 
 gulp.task('js', function() {
 	return gulp.src('src/*.js')
@@ -12,6 +15,16 @@ gulp.task('js', function() {
 		.pipe(gulp.dest('dist/'));
 });
 
+gulp.task('scss', function() {
+	return gulp.src('src/*.scss')
+		.pipe(sass())
+		.pipe(postCSS([
+			autoprefixer()
+		]))
+		.pipe(gulp.dest('dist/'));
+});
+
 gulp.task('default', function() {
 	gulp.watch('src/*.js', gulp.series(['js']));
+	gulp.watch('src/*.scss', gulp.series(['scss']));
 });
